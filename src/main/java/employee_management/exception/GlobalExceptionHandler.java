2b5f8ad6e168f95a43e9c00bdfd66625c1ce1dcd
Shuja Hasan <shujahassan655@gmail.com>
2026-07-21 12:50:57 +0500

    feat(exception): add global exception handling for employee endpoints

---
+++ src/main/java/employee_management/exception/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package employee_management.exception;
+
+import employee_management.dto.ErrorResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(EmployeeNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleEmployeeNotFound(
+            EmployeeNotFoundException ex, HttpServletRequest request){
+
+        ErrorResponse error = new ErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.NOT_FOUND.value(),
+                ex.getMessage(),
+                request.getRequestURI()
+        );
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+
+
+
+    }
+
+}
