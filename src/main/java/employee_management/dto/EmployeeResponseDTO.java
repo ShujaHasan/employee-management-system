2a979e659cee8813b91b3cbdf8678f16297d6ae2
Shuja Hasan <shujahassan655@gmail.com>
2026-07-22 11:17:07 +0500

    feat: add EmployeeResponseDTO for API responses

---
+++ src/main/java/employee_management/dto/EmployeeResponseDTO.java
@@ -0,0 +1,65 @@
+package employee_management.dto;
+
+public class EmployeeResponseDTO {
+
+    private Integer id;
+    private String name;
+    private String email;
+    private String department;
+    private String role;
+
+    public EmployeeResponseDTO() {
+    }
+
+    public EmployeeResponseDTO(Integer id, String name, String email, String department, String role) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.department = department;
+        this.role = role;
+    }
+
+    // Generate getters and setters
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+    
+}
